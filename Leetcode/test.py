7349e172f3b2a96e3cdc51ba9c38dc43bdfa0da5
aditya109 <adikid1996@gmail.com>
2020-06-02 10:05:58 +0530

    work till 2 june
    Signed-off-by: aditya109 <adikid1996@gmail.com>

--- Leetcode/test.py
+++ Leetcode/test.py
@@ -1,15 +1,21 @@
-from heapq import heappop, heappush, heapify 
-  
-min_heap = []
-heapify(min_heap)
+# Python3 Program to check whether a 
+# given key already exists in a dictionary. 
 
-heappush(min_heap, 4)
-heappush(min_heap, 2)
-heappush(min_heap, 5)
-heappush(min_heap, 7)
+# Function to print sum 
+def checkKey(dict, key): 
+	
+	if dict.has_key(key): 
+		print ("Present, value =", dict[key] )
+	else: 
+		print ("Not present")
 
-print(heappop(min_heap))
-print(heappop(min_heap))
-print(heappop(min_heap))
-print(heappop(min_heap))
+# Driver Function 
+dict = {'a': 100, 'b':200, 'c':300} 
+key = 'b'
+checkKey(dict, key) 
+
+key = 'w'
+checkKey(dict, key) 
+
+
 
