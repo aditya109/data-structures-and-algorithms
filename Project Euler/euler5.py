68f641b8dce6e8b4676a2878348ca9ca214bcf17
aditya109 <adikid1996@gmail.com>
2020-06-03 11:42:55 +0530

    work until 3 june
    Signed-off-by: aditya109 <adikid1996@gmail.com>

--- Project Euler/euler5.py
+++ Project Euler/euler5.py
@@ -1,7 +1,8 @@
+import math
 def smallest_possible_evenly_divisible_number(upper:int, lower:int):
-    pass
-
-
-
+    ans = 1
+    for i in range(lower, upper+1):
+        ans = int((ans*i)/math.gcd(ans, i))
+    return ans
 
 print(smallest_possible_evenly_divisible_number(20, 1))
